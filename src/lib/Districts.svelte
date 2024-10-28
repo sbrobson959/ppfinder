721c130c5fab0847062bc2e50e5fabde3a3f2e47
Sam <sbrobson@crimson.ua.edu>
2024-10-28 08:51:14 -0500

    fixed width on districts table on small devices

--- src/lib/Districts.svelte
+++ src/lib/Districts.svelte
@@ -5,7 +5,7 @@
 <h2 class="text-2xl font-bold mt-8">Political Districts</h2>
 <hr class="mb-5 mt-2 h-0.5 bg-zinc-100" />
 <div class="flex mb-5">
-	<table class="min-w-96 grow text-lg font-medium">
+	<table class=" grow text-lg font-medium">
 		<tbody>
 			<tr>
 				<td class="w-80 border-2 px-2">County</td>
@@ -30,8 +30,3 @@
 		</tbody>
 	</table>
 </div>
-<!-- 
-<td>US Congressional District</td>
-<td>County Commissioner District</td>
-<td>State House District</td>
-<td>State Senate District</td> -->
